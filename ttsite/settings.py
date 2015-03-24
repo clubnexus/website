f3a69f46ef20e662a1652fc5211e42c14d109af4
loblao <12ksit@gmail.com>
2015-03-24 16:57:42 -0300

    Fucking typo

--- ttsite/settings.py
+++ ttsite/settings.py
@@ -97,7 +97,7 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_USER', 'clubnexus1@gmail.com')
-    EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSOWORD']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
     EMAIL_PORT = 587
 
 # API
