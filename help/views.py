4cef2859041c95b00e724499637b8752caa1cefb
loblao <12ksit@gmail.com>
2015-03-26 17:35:03 -0300

    Help: Add more minigames

--- help/views.py
+++ help/views.py
@@ -19,7 +19,9 @@ def TT_help_screenshots(request):
     gamelist = ''
     for game, obj in (('Toon Puzzle', 'game'), ('Toon Puzzle 2', 'game2'),
                       ('Toon Shoot', 'toonshoot'), ('Cog Crusher', 'cogCrush'),
-                      ('TuneTown', 'tunetown')):
+                      ('TuneTown', 'tunetown'), ('Card Cog', 'cardcog'),
+                      ('Memory Game', 'memory'), ('Laff Lanes', 'laff'),
+                      ('Cog Invasion (Squirt)', 'coginv1'), ('Cog Invasion (Throw)', 'coginv2')):
         obj = static('shockwave/%s.swf' % obj)
         gamelist += '''<tr><td colspan="3" align="center"><br><font face="arial,helvetica" size="2">
                        <a href="%(obj)s">%(game)s</a></font></td></tr>''' % locals()
