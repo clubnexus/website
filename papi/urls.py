7d1a8a11e21201d6a7d60fffc331b59521a7322b
loblao <12ksit@gmail.com>
2015-03-28 13:17:27 -0300

    Fix error

--- papi/urls.py
+++ papi/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^login/$', views.login, name='login')
+    url(r'^login/$', views.PAPI_login, name='login')
 )
--- papi/views.py
+++ papi/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate
 from users.models import UserExt
 from users import events
 
-def TT_login(request):
+def PAPI_login(request):
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     # TO DO
